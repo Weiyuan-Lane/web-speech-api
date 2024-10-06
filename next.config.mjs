b0d6644c4cd92987962b851c958053840cbfbeff
Weiyuan <weiyuan.lane@gmail.com>
2024-10-06 17:22:43 +0800

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,47 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// import path from 'path';
+// // import { fileURLToPath } from 'url';
+// // const __filename = fileURLToPath(import.meta.url);
+// // const __dirname = path.dirname(__filename);
+
+/*****************************************************************************
+ * SET YOUR CONFIGURATION HERE
+ *****************************************************************************/
+
+// Set base path if your static app does not start from root
+const basePath = '/web-speech-api';
+
+/*****************************************************************************
+ *****************************************************************************/
+
+const webpackBasePath = process.env.SPA_EXP_BUILD === 'true' ? basePath : ''
+
+const nextConfig = {
+  basePath: webpackBasePath,
+  assetPrefix: webpackBasePath,
+
+  // webpack: function(config) {
+  //   config.module.rules.push(
+  //     {
+  //       test: /\.(jpe?g|png|gif|svg|ico)$/,
+  //       use: {
+  //         loader: 'url-loader',
+  //         options: {
+  //           limit: 8192,
+  //           fallback: 'file-loader',
+  //           publicPath: `${webpackBasePath}/_next/static/images/`,
+  //           outputPath: 'static/images/',
+  //           name: '[name].[hash:15].[ext]',
+  //         },
+  //       },
+  //     }
+  //   );
+
+  //   config.resolve.alias['@styles'] = path.join(__dirname, 'styles');
+  //   config.resolve.alias['@helpers'] = path.join(__dirname, 'helpers');
+  //   config.resolve.alias['@images'] = path.join(__dirname, 'images');
+
+  //   return config;
+  // },
+};
 
 export default nextConfig;
