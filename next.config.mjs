2d5fbe18ae4bd62d8d8fdf8823125381662645d2
Weiyuan <weiyuan.lane@gmail.com>
2024-10-06 23:02:30 +0800

    [Ops] Add experimental build flag

--- next.config.mjs
+++ next.config.mjs
@@ -27,30 +27,9 @@ const nextConfig = {
   images: {
     unoptimized: true
   },
-
-  // webpack: function(config) {
-  //   config.module.rules.push(
-  //     {
-  //       test: /\.(jpe?g|png|gif|svg|ico)$/,
-  //       use: {
-  //         loader: 'url-loader',
-  //         options: {
-  //           limit: 8192,
-  //           fallback: 'file-loader',
-  //           publicPath: `${webpackBasePath}/_next/static/images/`,
-  //           outputPath: 'static/images/',
-  //           name: '[name].[hash:15].[ext]',
-  //         },
-  //       },
-  //     }
-  //   );
-
-  //   config.resolve.alias['@styles'] = path.join(__dirname, 'styles');
-  //   config.resolve.alias['@helpers'] = path.join(__dirname, 'helpers');
-  //   config.resolve.alias['@images'] = path.join(__dirname, 'images');
-
-  //   return config;
-  // },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
 };
 
 export default nextConfig;
