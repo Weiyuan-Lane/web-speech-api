4e3ad6a1b86319da89929530d6272b6cb3e59fff
Weiyuan <weiyuan.lane@gmail.com>
2024-10-06 23:42:56 +0800

    [Ops] Update build config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,3 @@
-// import path from 'path';
-// // import { fileURLToPath } from 'url';
-// // const __filename = fileURLToPath(import.meta.url);
-// // const __dirname = path.dirname(__filename);
-
 /**
  * @type {import('next').NextConfig}
  */
